5471ea42099f4eafaa899b9cc0cbe35689301895
anggiirawan13 <irawananggi76@gmail.com>
2024-06-30 02:46:23 +0700

    init

--- be/src/main/java/com/be/BeApplication.java
+++ be/src/main/java/com/be/BeApplication.java
@@ -2,12 +2,57 @@ package com.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
 public class BeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(com.be.BeApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.be.app.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.ignoredParameterTypes(org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController.class)
+				.securitySchemes(List.of(apiKey()))
+				.securityContexts(Collections.singletonList(securityContext()));
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Bearer", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return List.of(new SecurityReference("Bearer", authorizationScopes));
 	}
 
 }
